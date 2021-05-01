28005c7d325c850958585512c9f6a5d970f479a5
Jim Strother <jim.strother@gmail.com>
2021-05-01 16:30:38 -0500

    Learning Range() w/ step values

--- ProgramFlow/strings2.py
+++ ProgramFlow/strings2.py
@@ -1,6 +1,11 @@
-number = "9,223;372:036 854,775;807"
-seperators = number[1::4]
-print(seperators)
+number = input("Please enter a series of numbers using any seperators you like: ")
+seperators = ""
+
+for char in number:
+  if not char.isnumeric():
+    seperators = seperators + char
+
+# print(seperators)
 
 values = "".join(char if char not in seperators else " " for char in number).split()
-print([int(val) for val in values])
+print(sum([int(val) for val in values]))
